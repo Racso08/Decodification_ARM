7cafe7af418ad0703e43d909f8044f422360a154
Kevin <skryfall@gmail.com>
2020-08-04 10:05:18 -0600

    Correccion del codigo arm

--- ARM Code/Decodificador.s
+++ ARM Code/Decodificador.s
@@ -13,7 +13,7 @@ Fin		MOV		R0, #0x5200 ; Carga dirección de memoria del Flip Flop en R0
 		;		Rutina que se encarga de verificar cual proceso de decodificación a realizar
 		
 TipoDeco	MOV		R0, #0x4000 ; Carga dirección de memoria del ROM en R0
-		LDR		R1, [R0] ; Carga el primer byte a R1, este indica el tipo de decodificación a realizar
+		LDRB		R1, [R0] ; Carga el primer byte a R1, este indica el tipo de decodificación a realizar
 		CMP		R1, #0   ; Se verifica si el byte es 0
 		BEQ		DecoXOR  ; Si lo es, inicia el proceso de Decodificación XOR
 		CMP		R1, #1   ; Se verifica si el byte es 1
@@ -25,49 +25,49 @@ TipoDeco	MOV		R0, #0x4000 ; Carga dirección de memoria del ROM en R0
 		;		Rutina que se encarga de realizar la decodificación XOR
 		
 DecoXOR	MOV		R1, #0x4600 ; Carga dirección de memoria del RAM en R1
-		MOV		R2, #4   ; Almacena un 4 en R2
+		MOV		R2, #1   ; Almacena un 1 en R2
 		MOV		R3, #0   ; Almacena un 0 en R3
-		LDR		R4, [R0, R2] ; Carga el segundo byte del ROM a R4, esta es la llave del XOR
-		ADD		R2, R2, #4   ; Suma 4 a R2
+		LDRB		R4, [R0, R2] ; Carga el segundo byte del ROM a R4, esta es la llave del XOR
+		ADD		R2, R2, #1   ; Suma 1 a R2
 		
-CicloXOR	LDR		R5, [R0, R2] ; Carga un byte del mensaje codificado a R5
-		CMP		R5, #0xFF     ; Verifica si el byte es 0xFF
+CicloXOR	LDRB		R5, [R0, R2] ; Carga un byte del mensaje codificado a R5
+		CMP		R5, #0xFF    ; Verifica si el byte es 0xFF
 		BEQ		Fin	        ; Si es 0xFF, el mensaje acabó y vuelve al inicio
-		ADD		R2, R2, #4   ; Suma 4 a R2
+		ADD		R2, R2, #1   ; Suma 1 a R2
 		EOR		R6, R5, R4   ; Aplica XOR entre la letra y la llave
-		STR		R6, [R1, R3] ; Almacena la letra decodificada en la RAM
-		ADD		R3, R3, #4   ; Suma 4 a R3
+		STRB		R6, [R1, R3] ; Almacena la letra decodificada en la RAM
+		ADD		R3, R3, #1   ; Suma 1 a R3
 		B		CicloXOR     ; Repite el ciclo
 		
 		;		Rutina que se encarga de realizar la decodificación NOT
 		
 DecoNOT	MOV		R1, #0x4600 ; Carga dirección de memoria del RAM en R1
-		MOV		R2, #4   ; Almacena un 4 en R2
+		MOV		R2, #1   ; Almacena un 1 en R2
 		MOV		R3, #0   ; Almacena un 0 en R3
 		MOV		R4, #0xFF ; Carga un 0xFF a R4, esto para poder realizar la negación utilizando XOR
 		
-CicloNOT	LDR		R5, [R0, R2] ; Carga un byte del mensaje codificado a R5
-		CMP		R5, #0xFF     ; Verifica si el byte es 0xFF
+CicloNOT	LDRB		R5, [R0, R2] ; Carga un byte del mensaje codificado a R5
+		CMP		R5, #0xFF    ; Verifica si el byte es 0xFF
 		BEQ		Fin	        ; Si es 0xFF, el mensaje acabó y vuelve al inicio
-		ADD		R2, R2, #4   ; Suma 4 a R2
+		ADD		R2, R2, #1   ; Suma 1 a R2
 		EOR		R6, R5, R4   ; Aplica XOR entre la letra y 0xFF, para negar los bits de R4
-		STR		R6, [R1, R3] ; Almacena la letra decodificada en la RAM
-		ADD		R3, R3, #4   ; Suma 4 a R3
+		STRB		R6, [R1, R3] ; Almacena la letra decodificada en la RAM
+		ADD		R3, R3, #1   ; Suma 1 a R3
 		B		CicloNOT     ; Repite el ciclo
 		
 		;		Rutina que se encarga de realizar la decodificación ROR
 		
 DecoROR	MOV		R1, #0x4600 ; Carga dirección de memoria del RAM en R1
-		MOV		R2, #4   ; Almacena un 4 en R2
+		MOV		R2, #1   ; Almacena un 1 en R2
 		MOV		R3, #0   ; Almacena un 0 en R3
-		LDR		R4, [R0, R2] ; Carga el segundo byte del ROM a R4, esta es la cantidad de bits a rotar
-		ADD		R2, R2, #4   ; Suma 4 a R2
+		LDRB		R4, [R0, R2] ; Carga el segundo byte del ROM a R4, esta es la cantidad de bits a rotar
+		ADD		R2, R2, #1   ; Suma 1 a R2
 		
-CicloROR	LDR		R5, [R0, R2] ; Carga un byte del mensaje codificado a R5
-		CMP		R5, #0xFF     ; Verifica si el byte es 0xFF
+CicloROR	LDRB		R5, [R0, R2] ; Carga un byte del mensaje codificado a R5
+		CMP		R5, #0xFF    ; Verifica si el byte es 0xFF
 		BEQ		Fin	        ; Si es 0xFF, el mensaje acabó y vuelve al inicio
-		ADD		R2, R2, #4   ; Suma 4 a R2
+		ADD		R2, R2, #1   ; Suma 1 a R2
 		ROR		R6, R5, R4   ; Aplica ROR entre la letra y la llave
-		STR		R6, [R1, R3] ; Almacena la letra decodificada en la RAM
-		ADD		R3, R3, #4   ; Suma 4 a R3
+		STRB		R6, [R1, R3] ; Almacena la letra decodificada en la RAM
+		ADD		R3, R3, #1   ; Suma 1 a R3
 		B		CicloROR     ; Repite el ciclo
--- ARM Code/DecodificadorSinComentarios.s
+++ ARM Code/DecodificadorSinComentarios.s
@@ -7,49 +7,49 @@ Fin:		MOV		R0, #0x5200
 		STR		R1, [R0]
 		B		Inicio	
 TipoDeco:	MOV		R0, #0x4000
-		LDR		R1, [R0]
+		LDRB		R1, [R0]
 		CMP		R1, #0
 		BEQ		DecoXOR
 		CMP		R1, #1
 		BEQ		DecoNOT
 		CMP		R1, #3
 		BEQ		DecoROR
-		B		Inicio
+		B		Inicio	
 DecoXOR:	MOV		R1, #0x4600
-		MOV		R2, #4
+		MOV		R2, #1
 		MOV		R3, #0
-		LDR		R4, [R0, R2]
-		ADD		R2, R2, #4
-CicloXOR:	LDR		R5, [R0, R2]
+		LDRB		R4, [R0, R2]
+		ADD		R2, R2, #1	
+CicloXOR:	LDRB		R5, [R0, R2]
 		CMP		R5, #0xFF
 		BEQ		Fin
-		ADD		R2, R2, #4
+		ADD		R2, R2, #1
 		EOR		R6, R5, R4
-		STR		R6, [R1, R3]
-		ADD		R3, R3, #4
-		B		CicloXOR
+		STRB		R6, [R1, R3]
+		ADD		R3, R3, #1
+		B		CicloXOR	
 DecoNOT:	MOV		R1, #0x4600
-		MOV		R2, #4
+		MOV		R2, #1
 		MOV		R3, #0
 		MOV		R4, #0xFF
-CicloNOT:	LDR		R5, [R0, R2]
+CicloNOT:	LDRB		R5, [R0, R2]
 		CMP		R5, #0xFF
 		BEQ		Fin
-		ADD		R2, R2, #4
+		ADD		R2, R2, #1
 		EOR		R6, R5, R4
-		STR		R6, [R1, R3]
-		ADD		R3, R3, #4
+		STRB		R6, [R1, R3]
+		ADD		R3, R3, #1
 		B		CicloNOT
 DecoROR:	MOV		R1, #0x4600
-		MOV		R2, #4
+		MOV		R2, #1
 		MOV		R3, #0
-		LDR		R4, [R0, R2]
-		ADD		R2, R2, #4
-CicloROR:	LDR		R5, [R0, R2]
+		LDRB		R4, [R0, R2]
+		ADD		R2, R2, #1
+CicloROR:	LDRB		R5, [R0, R2]
 		CMP		R5, #0xFF
 		BEQ		Fin
-		ADD		R2, R2, #4
+		ADD		R2, R2, #1
 		ROR		R6, R5, R4
-		STR		R6, [R1, R3]
-		ADD		R3, R3, #4
+		STRB		R6, [R1, R3]
+		ADD		R3, R3, #1
 		B		CicloROR

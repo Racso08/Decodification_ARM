695189606dc1920594ea98d7841729596a13cb79
Kevin <skryfall@gmail.com>
2020-08-05 14:14:26 -0600

    Ahora escribe el mensaje en un txt
    Finalizado

--- Modules/top.sv
+++ Modules/top.sv
@@ -1,3 +1,4 @@
+`timescale 1 ps / 1 ps
 module top (input logic clk, reset, set,
 				output logic [31:0] WriteData, DataAdr,
 				output logic MemWrite);
@@ -17,9 +18,25 @@ module top (input logic clk, reset, set,
 	chipset #(32) chipset(DataAdr, MemWrite, WEram, WEFF, select);
 	
 	rom rom(romAddress[7:0], romData);
-	ram ram(clk, WEram, ramAddress[7:0], WriteData, ramData);
+	ram ram(clk, WEram, ramAddress[7:0], WriteData[7:0], ramData);
 	register #(32) register(clk, reset, set, WEFF, WriteData, ffData);
 	
 	mux2a4 #(32) Mux({24'b0, romData}, {24'b0, ramData}, ffData, select, ReadData);
+	
+	int fd;
+	assign fd = $fopen ("./Mensaje.txt", "w");
+	always begin
+		#5 if (DataAdr >= 'h4600 && DataAdr <= 'h5000 && WEram == 1'b1) begin
+			$fdisplay (fd, "s = %s", ramData);
+			end
+	end
+
+	always begin	
+		#25 if (DataAdr >= 'h5200 && ffData == 32'b0) begin
+				$fclose(fd);
+				$finish;
+			end
+
+	end
 
 endmodule 
--- Tests/testbench.sv
+++ Tests/testbench.sv
@@ -1,27 +1,27 @@
 `timescale 1 ps / 1 ps
 module testbench();
 
-logic clk;
-logic reset;
-logic set;
-logic [31:0] WriteData, DataAdr;
-logic MemWrite;
+	logic clk;
+	logic reset;
+	logic set;
+	logic [31:0] WriteData, DataAdr;
+	logic MemWrite;
 
-// instantiate device to be tested
-top dut(clk, reset, set, WriteData, DataAdr, MemWrite);
-
-// initialize test
-initial
-	begin
-		reset <= 1; # 5; reset <= 0;
-		set <= 1; # 5; set <= 0;
-	end
+	// instantiate device to be tested
+	top dut(clk, reset, set, WriteData, DataAdr, MemWrite);
 	
-// generate clock to sequence tests
-always
-	begin
-		clk <= 1; # 5; 
-		clk <= 0; # 5;
-	end 
+	// initialize test
+	initial
+		begin
+			reset <= 1; # 5; reset <= 0;
+			set <= 1; # 5; set <= 0;
+		end
+	
+	// generate clock to sequence tests
+	always
+		begin
+			clk <= 1; # 5; 
+			clk <= 0; # 5;
+		end 
 
 endmodule 	
